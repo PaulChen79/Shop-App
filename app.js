8420763cfedfa2db3bba1e478b64fafbde226710
Pin Hsu Chen <paul791111@gmail.com>
2022-06-05 21:16:00 +0800

    feat: add passport

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
+const passport = require('./config/passport')
 const routes = require('./routes/index')
 const PORT = process.env.PORT || 3000
 
@@ -14,12 +15,15 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(methodOverride('_method'))
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
   res.locals.error_msg = req.flash('error_msg')
+  res.locals.user = req.user
   next()
 })
 
---
+++ config/passport.js
@@ -0,0 +1,35 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const { User } = require('../models')
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, email, password, done) => {
+  try {
+    const user = await User.findOne({ where: { email } })
+    if (!user) return done(null, false, req.flash('warning_msg', 'Email or password incorrect.'))
+    const isMatched = await bcrypt.compare(password, user.password)
+    if (!isMatched) return done(null, false, req.flash('warning_msg', 'Email or password incorrect.'))
+    return done(null, user)
+  } catch (error) {
+    done(error, false)
+  }
+}))
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findByPk(id)
+    done(null, user)
+  } catch (error) {
+    done(error, false)
+  }
+})
+
+module.exports = passport
