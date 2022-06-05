0a1ea13a2a7e2d60e966a14117f9512821dea10f
Pin Hsu Chen <paul791111@gmail.com>
2022-06-05 18:06:36 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+const session = require('express-session')
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+const exphbs = require('express-handlebars')
+const routes = require('./routes/index')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(methodOverride('_method'))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`)
+})
