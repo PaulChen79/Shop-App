744b3b608bc6396ab4f870aa299308f7e9f366a4
Pin Hsu Chen <paul791111@gmail.com>
2022-06-06 21:28:53 +0800

    feat: add admin products & categories page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,7 +1,38 @@
+const { Product, SKU, Category } = require('../models')
 const adminController = {
   getProductsPage: async (req, res, next) => {
     try {
-      res.render('admin/products')
+      let products = await Product.findAll({
+        include: [
+          SKU,
+          {
+            model: Category,
+            include: {
+              model: Category,
+              as: 'subCategories',
+              required: false
+            }
+          }
+        ]
+      })
+      products = await products.map(product => product.toJSON())
+      res.render('admin/products', { products })
+    } catch (error) {
+      next(error)
+    }
+  },
+  getCategoriesPage: async (req, res, next) => {
+    try {
+      let categories = await Category.findAll({
+        where: { parentId: null },
+        include: {
+          model: Category,
+          as: 'subCategories',
+          required: false
+        }
+      })
+      categories = await categories.map(category => category.toJSON())
+      return res.render('admin/categories', { categories })
     } catch (error) {
       next(error)
     }
--- routes/admin/admin-routes.js
+++ routes/admin/admin-routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/products', adminController.getProductsPage)
+router.get('/categories', adminController.getCategoriesPage)
 
 module.exports = router
